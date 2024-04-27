5803fbba40892cf2da04903e93826a91fdb2888f
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-04-27 16:04:10 +0530

    Update `arduino_code.ino`

--- Arduino/arduino_code.ino
+++ Arduino/arduino_code.ino
@@ -22,149 +22,152 @@ bool parcelIsExpected = true;
 
 void unlockDoor()
 {
-	if(!doorIsOpen && !doorIsClosedPerm)
-	{
-		for(int deg = 0 ; deg <= 90 ; deg += 10)
-		{
-			servoDoor.write(deg);
-			delay(10);
-		}
-		
-		digitalWrite(LED_DOOR_GREEN_PIN, HIGH);
-		digitalWrite(LED_DOOR_RED_PIN, LOW);
-		doorIsOpen = true;
-	}
+  if(!doorIsOpen && !doorIsClosedPerm)
+  {
+    for(int deg = 0 ; deg <= 90 ; deg += 10)
+    {
+      servoDoor.write(deg);
+      delay(10);
+    }
+    
+    digitalWrite(LED_DOOR_GREEN_PIN, HIGH);
+    digitalWrite(LED_DOOR_RED_PIN, LOW);
+    doorIsOpen = true;
+  }
 }
 
 void lockDoor()
 {
-	if(doorIsOpen)
-	{
-		for(int deg = 90 ; deg >= 0 ; deg -= 10)
-		{
-			servoDoor.write(deg);
-			delay(10);
-		}
-		
-		digitalWrite(LED_DOOR_GREEN_PIN, LOW);
-		digitalWrite(LED_DOOR_RED_PIN, HIGH);
-		doorIsOpen = false;
-	}
+  if(doorIsOpen)
+  {
+    for(int deg = 90 ; deg >= 0 ; deg -= 10)
+    {
+      servoDoor.write(deg);
+      delay(10);
+    }
+    
+    digitalWrite(LED_DOOR_GREEN_PIN, LOW);
+    digitalWrite(LED_DOOR_RED_PIN, HIGH);
+    doorIsOpen = false;
+  }
 }
 
 void unlockParcelBox()
 {
-	if(parcelIsExpected)
-	{
-		for(int deg = 0 ; deg <= 90 ; deg += 10)
-		{
-			servoParcel.write(deg);
-			delay(10);
-		}
-		
-		digitalWrite(LED_PARCEL_GREEN_PIN, HIGH);
-		digitalWrite(LED_PARCEL_RED_PIN, LOW);
-		
-		delay(5000);
-		
-		for(int deg = 90 ; deg >= 0 ; deg -= 10)
-		{
-			servoParcel.write(deg);
-			delay(10);
-		}
-		
-		digitalWrite(LED_PARCEL_GREEN_PIN, LOW);
-		digitalWrite(LED_PARCEL_RED_PIN, HIGH);
-	}
+  if(parcelIsExpected)
+  {
+    for(int deg = 0 ; deg <= 90 ; deg += 10)
+    {
+      servoParcel.write(deg);
+      delay(10);
+    }
+    
+    digitalWrite(LED_PARCEL_GREEN_PIN, HIGH);
+    digitalWrite(LED_PARCEL_RED_PIN, LOW);
+    
+    delay(5000);
+    
+    for(int deg = 90 ; deg >= 0 ; deg -= 10)
+    {
+      servoParcel.write(deg);
+      delay(10);
+    }
+    
+    digitalWrite(LED_PARCEL_GREEN_PIN, LOW);
+    digitalWrite(LED_PARCEL_RED_PIN, HIGH);
+  }
 }
 
 void activateBuzzer(int duration)
 {
-	digitalWrite(BUZZER_PIN, HIGH);
-	delay(duration);
-	digitalWrite(BUZZER_PIN, LOW);
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(duration);
+  digitalWrite(BUZZER_PIN, LOW);
 }
 
 int measureDoorDistance()
 {
-	digitalWrite(TRIGGER_PIN, LOW);
-	delayMicroseconds(2);
-	
-	digitalWrite(TRIGGER_PIN, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(TRIGGER_PIN, LOW);
-	
-	time = pulseIn(ECHO_PIN, HIGH);
-	distance = time * 0.0343 / 2; // Unit -> centimeters
-	return distance;
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  
+  time = pulseIn(ECHO_PIN, HIGH);
+  distance = time * 0.0343 / 2; // Unit -> centimeters
+  return distance;
 }
 
 void setup()
 {
-	servoDoor.attach(SERVO_DOOR_PIN);
-	servoParcel.attach(SERVO_PARCEL_PIN);
-	
-	pinMode(BUZZER_PIN, OUTPUT);
-	pinMode(ECHO_PIN, INPUT);
-	pinMode(TRIGGER_PIN, OUTPUT);
-	pinMode(LED_DOOR_GREEN_PIN, OUTPUT);
-	pinMode(LED_DOOR_RED_PIN, OUTPUT);
-	pinMode(LED_PARCEL_GREEN_PIN, OUTPUT);
-	pinMode(LED_PARCEL_RED_PIN, OUTPUT);
-	
-	Serial.begin(9600);
+  servoDoor.attach(SERVO_DOOR_PIN);
+  servoParcel.attach(SERVO_PARCEL_PIN);
+  
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(LED_DOOR_GREEN_PIN, OUTPUT);
+  pinMode(LED_DOOR_RED_PIN, OUTPUT);
+  pinMode(LED_PARCEL_GREEN_PIN, OUTPUT);
+  pinMode(LED_PARCEL_RED_PIN, OUTPUT);
+
+  digitalWrite(LED_DOOR_RED_PIN, HIGH);
+  digitalWrite(LED_PARCEL_RED_PIN, HIGH);
+  
+  Serial.begin(9600);
 }
 
 void loop()
 {
-	if(Serial.available())
-	{
-		serialInput = Serial.read();
-		
-		switch(serialInput)
-		{
-			case '0':
-				lockDoor();
-				break;
-			
-			case '1':
-				unlockDoor();
-				break;
-			
-			case '2':
-				lockDoor();
-				break;
-			
-			case '3':
-				activateBuzzer(3000);
-				break;
-			
-			case '4':
-				doorIsClosedPerm = !doorIsClosedPerm;
-				break;
-			
-			case '5':
-				parcelIsExpected = !parcelIsExpected;
-				break;
-			
-			case '6':
-				doorIsClosedPerm = !doorIsClosedPerm;
-				parcelIsExpected = !parcelIsExpected;
-				break;
-			
-			case '7':
-				unlockDoor();
-				delay(5000);
-				lockDoor();
-				break;
-			
-			case '8':
-				unlockParcelBox();
-				break;
-		}
-	}
-	
-	distance = measureDoorDistance();
-	if(distance <= 5)
-		activateBuzzer(100);
+  if(Serial.available())
+  {
+    serialInput = Serial.read();
+    
+    switch(serialInput)
+    {
+      case '0':
+        lockDoor();
+        break;
+      
+      case '1':
+        unlockDoor();
+        break;
+      
+      case '2':
+        lockDoor();
+        break;
+      
+      case '3':
+        activateBuzzer(3000);
+        break;
+      
+      case '4':
+        doorIsClosedPerm = !doorIsClosedPerm;
+        break;
+      
+      case '5':
+        parcelIsExpected = !parcelIsExpected;
+        break;
+      
+      case '6':
+        doorIsClosedPerm = !doorIsClosedPerm;
+        parcelIsExpected = !parcelIsExpected;
+        break;
+      
+      case '7':
+        unlockDoor();
+        delay(5000);
+        lockDoor();
+        break;
+      
+      case '8':
+        unlockParcelBox();
+        break;
+    }
+  }
+  
+  distance = measureDoorDistance();
+  if(distance <= 15 && !doorIsOpen && distance != 0)
+    activateBuzzer(100);
 }
